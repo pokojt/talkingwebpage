db339ac4d399cbfdba53871780b61eab3c3d39d4
Taylor Pokoj and Tyler Suderman <pokojt@gmail.com>
2016-01-12 09:27:26 -0800

    Add JS effects and event listeners.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,45 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>What's up!?</li>");
     $("ul#webpage").prepend("<li>Just Chillin'</li>");
-    $("li").css('background-color', 'green');
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+      $("ul#webpage").children("li").first().remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+      $("ul#user").children("li").first().remove();
+    });
   });
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Catch ya later!</li>");
     $("ul#webpage").prepend("<li>Leaving so soon!?</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+      $("ul#webpage").children("li").first().remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+      $("ul#user").children("li").first().remove();
+    });
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>wie geht's?</li>");
     $("ul#webpage").prepend("<li>Oh you speak German? You fancy.</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+      $("ul#webpage").children("li").first().remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+      $("ul#user").children("li").first().remove();
+    });
   });
 });
